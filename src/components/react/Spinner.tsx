a19ba384b80d0f14c4ccd0432ece4ad1a65ef586
saphalpdyl <saphalpdyl@gmail.com>
2024-10-29 00:36:29 -0500

    add: top languages and image loading

--- src/components/react/Spinner.tsx
+++ src/components/react/Spinner.tsx
@@ -1,6 +1,6 @@
 function Spinner() {
   return <div className="flex items-center justify-center">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
+    <div className="animate-spin rounded-full h-6 w-6 border-t-4 border-b-4 border-gray-500"></div>
   </div>;
 }
 
---
+++ src/components/react/TopLanguages.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { actions } from "astro:actions";
+
+import Spinner from "./Spinner";
+
+const PROGRESS_BAR_TOTAL_LENGTH = 10;
+const TOP_LANGUAGES = 6;
+
+function ProgressBar({ percentage } : {
+  percentage: number,
+}) {
+  const filledBlocks = Math.round((percentage / 100) * PROGRESS_BAR_TOTAL_LENGTH);
+  const emptyBlocks = PROGRESS_BAR_TOTAL_LENGTH - filledBlocks;
+  
+  return (
+    <div className="font-mono text-zinc-500">
+      [{'█'.repeat(filledBlocks)}
+      {'░'.repeat(emptyBlocks)}]
+      <span className="font-serif italic">{percentage.toFixed(2)}%</span>
+    </div>
+  );
+}
+
+function TopLanguages({ githubLogo }: {
+  githubLogo?: React.ReactNode,
+}) {
+  const [languages, setLanguages] = useState<null | string[][]>(null);
+
+  async function _refreshLanguages() {
+    const data = await actions.getTopLanguages({
+      top: 10,
+    });
+    if (data == undefined)
+      throw new Error("Data is undefined");
+    
+    setLanguages(data.data!);
+  }
+  
+  useEffect(() => {
+    _refreshLanguages();
+  }, []);
+
+  if (!languages) return <Spinner />
+
+  return (
+    <div className="flex flex-col font-serif font-semibold">
+      <div className="flex gap-2 justify-center items-center">
+        Most Used Languages on GitHub 
+        <div>
+          { githubLogo }
+        </div>
+      </div>
+      <div className="grid grid-cols-2 font-serif">
+        {
+          languages && languages.slice(0,TOP_LANGUAGES).map(([language, percentage]) => {
+            return <>
+              {`${language.padEnd(30)}: `}
+              <ProgressBar percentage={parseFloat(percentage)} />
+            </>
+          })
+        }
+      </div>
+    </div>
+  ) 
+}
+
+export default TopLanguages;
