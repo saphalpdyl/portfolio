0d32aa9dedc0e2e84b0917c259798049c4d75382
saphalpdyl <saphalpdyl@gmail.com>
2024-10-31 13:33:07 -0500

    add: create a get busy status getting action

---
+++ src/actions/getBusyStatusAction.ts
@@ -0,0 +1,71 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+const DEFAULT_DAYS_AHEAD = 5;
+const DEFAULT_CALENDAR_ID = "saphalpdyl@gmail.com";
+
+export type FreeBusyStatus = {
+  status: "busy" | "free";
+} & (
+  { status: "busy", freeAt: Date | null}
+  | { status: "free"; busyAt: Date | null}
+);
+
+export default defineAction({
+  input: z.object({
+    daysAhead: z.number().min(1, 'cannot be less than 1').default(DEFAULT_DAYS_AHEAD),
+  }),
+  handler: async({ daysAhead }) : Promise<FreeBusyStatus>=> {
+    const timeMin = new Date();
+    timeMin.setDate(timeMin.getDate() - 1)
+    const timeMax = new Date();
+    timeMax.setDate(timeMax.getDate() + daysAhead);
+
+    const rawResponse = await fetch(`https://www.googleapis.com/calendar/v3/freeBusy?key=${import.meta.env.GOOGLE_CALENDAR_API_KEY}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        timeMin: timeMin.toISOString(),
+        timeMax: timeMax.toISOString(),
+        
+        items: [
+          {
+            id: DEFAULT_CALENDAR_ID,
+          }
+        ]
+      })
+    });
+
+
+    const response = await rawResponse.json();
+
+    const busySchedule: {
+      start: string,
+      end: string,
+    }[] = response.calendars[DEFAULT_CALENDAR_ID].busy;
+    
+    const now = new Date();
+
+    // Check if currently busy
+    const currentBusySlot = busySchedule.find(slot => 
+        now >= new Date(slot.start) && now <= new Date(slot.end)
+    );
+
+    if (currentBusySlot) {
+        // Currently busy
+        return {
+            status: 'busy',
+            freeAt: new Date(currentBusySlot.end),
+        };
+    } else {
+        // Currently free
+        const nextBusySlot = busySchedule.find(slot => new Date(slot.start) > now);
+        return {
+            status: 'free',
+            busyAt: nextBusySlot ? new Date(nextBusySlot.start) : null
+        };
+    }
+  }
+})
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,9 @@
+import getBusyStatusAction from "./getBusyStatusAction"
 import getTopLanguagesAction from "./getTopLanguagesAction"
 import getTopRepositoriesAction from "./getTopRepositoriesAction"
 
 export const server = {
   getTopLanguages: getTopLanguagesAction,
   getTopRepositories: getTopRepositoriesAction,
+  getBusyStatus: getBusyStatusAction,
 }
