576b013fb0f1276b9897048c70cbb24e7fd72c2b
saphalpdyl <saphalpdyl@gmail.com>
2024-11-02 23:52:02 -0500

    add: API endpoint to recieve process update data

---
+++ src/pages/api/refreshProcessInformation.ts
@@ -0,0 +1,32 @@
+import crypto from "node:crypto";
+
+import type { APIRoute } from "astro";
+import OpenProcessesDataManagerSingleton from "../../lib/OpenProcessesDataManager";
+
+export const POST: APIRoute = async ({ request }) => {
+  const dataManager = new OpenProcessesDataManagerSingleton();
+  
+  const body = await request.json();
+
+  // Verifying hash
+  const vertificationHmac = crypto.createHmac("sha256", import.meta.env.PROCESS_API_SECRET);
+  vertificationHmac.write(JSON.stringify(body.payload));
+  vertificationHmac.end();
+  const calculatedHash = vertificationHmac.read().toString("hex");
+
+  if ( calculatedHash !== body.hash ) {
+    console.error("ERROR: Couldn't verify hash. The data might have been tampered with.");
+    return new Response(JSON.stringify({
+      status: 400,
+      message: "Invalid hash signature"
+    }))
+  }
+
+  dataManager.setData(body.payload);
+  
+  return new Response(JSON.stringify({
+      status: 200,
+      message: "Data added successfully",
+    })
+  );
+}
