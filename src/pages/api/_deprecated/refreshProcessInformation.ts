e577ac3c1235e30c162ae8c31604fbb17fd349b6
saphalpdyl <saphalpdyl@gmail.com>
2025-06-04 23:28:43 -0500

    fix: commented deprecated code

--- src/pages/api/_deprecated/refreshProcessInformation.ts
+++ src/pages/api/_deprecated/refreshProcessInformation.ts
@@ -1,30 +1,30 @@
-import crypto from "node:crypto";
+// import crypto from "node:crypto";
 
-import type { APIRoute } from "astro";
-import OpenProcessesDataManagerSingleton from "../../lib/OpenProcessesDataManager";
+// import type { APIRoute } from "astro";
+// import OpenProcessesDataManagerSingleton from "../../lib/OpenProcessesDataManager";
 
-export const POST: APIRoute = async ({ request }) => {
-  const dataManager = OpenProcessesDataManagerSingleton.getInstance();
+// export const POST: APIRoute = async ({ request }) => {
+//   const dataManager = OpenProcessesDataManagerSingleton.getInstance();
   
-  const body = await request.json();
+//   const body = await request.json();
 
-  // Verifying hash
-  const vertificationHmac = crypto.createHmac("sha256", import.meta.env.PROCESS_API_SECRET);
-  vertificationHmac.write(JSON.stringify(body.payload));
-  vertificationHmac.end();
-  const calculatedHash = vertificationHmac.read().toString("hex");
+//   // Verifying hash
+//   const vertificationHmac = crypto.createHmac("sha256", import.meta.env.PROCESS_API_SECRET);
+//   vertificationHmac.write(JSON.stringify(body.payload));
+//   vertificationHmac.end();
+//   const calculatedHash = vertificationHmac.read().toString("hex");
 
-  if ( calculatedHash !== body.hash ) {
-    console.error("ERROR: Couldn't verify hash. The data might have been tampered with.");
-    return new Response(JSON.stringify({
-      status: 400,
-      message: "Invalid hash signature"
-    }))
-  }
+//   if ( calculatedHash !== body.hash ) {
+//     console.error("ERROR: Couldn't verify hash. The data might have been tampered with.");
+//     return new Response(JSON.stringify({
+//       status: 400,
+//       message: "Invalid hash signature"
+//     }))
+//   }
 
-  dataManager.setData(body.payload);
+//   dataManager.setData(body.payload);
   
-  return new Response(null, {
-    status: 204,
-  });
-}
+//   return new Response(null, {
+//     status: 204,
+//   });
+// }
